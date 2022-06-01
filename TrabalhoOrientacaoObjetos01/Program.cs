d6d45bc2ee738fdbe64451f898b95b81cda6b449
ana.wruck <ana.wruck@ACESSO06>
2022-06-01 19:10:31 -0300

    Add project files.

---
+++ TrabalhoOrientacaoObjetos01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(@"-------MENU-------
+1 - Questão 1
+2 - Questão 2
+3 - Questão 3
+4 - Questão 4");
+
+Console.Write("\nDigite a opção desejada: ");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDesejada == 1)
+{
+    Questao01 questao01 = new Questao01();
+    questao01.Executar();
+}
