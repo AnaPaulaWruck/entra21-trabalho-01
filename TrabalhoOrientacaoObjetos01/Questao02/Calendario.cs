7af519e1df0af13bad8b881c78082a13c5b25c1d
Amanda.Bauler <amanda.bauler@hotmail.com>
2022-06-01 21:57:10 -0300

    Atualização Questão 02, inclusão do método ObterDiaPorExtenso.

--- TrabalhoOrientacaoObjetos01/Questao02/Calendario.cs
+++ TrabalhoOrientacaoObjetos01/Questao02/Calendario.cs
@@ -9,62 +9,110 @@ namespace TrabalhoOrientacaoObjetos01.Questao02
     public class Calendario
     {
         public DateTime Data;
-
+        //Transformar a data e string.
         public string ObterMesPorExtenso()
         {
             var mes = Data.Month;
 
             if (mes == 1)
             {
-                return "Janeiro";
+                return "janeiro";
             }
             else if (mes == 2)
             {
-                return "Fevereiro";
+                return "fevereiro";
             }
             else if (mes == 3)
             {
-                return "Março";
+                return "março";
             }
             else if (mes == 4)
             {
-                return "Abril";
+                return "abril";
             }
             else if (mes == 5)
             {
-                return "Maio";
+                return "maio";
             }
             else if (mes == 6)
             {
-                return "Junho";
+                return "junho";
             }
             else if (mes == 7)
             {
-                return "Julho";
+                return "julho";
             }
             else if (mes == 8)
             {
-                return "Agosto";
+                return "agosto";
             }
             else if (mes == 9)
             {
-                return "Setembro";
+                return "setembro";
             }
             else if (mes == 10)
             {
-                return "Outubro";
+                return "outubro";
             }
             else if (mes == 11)
             {
-                return "Novembro";
+                return "novembro";
             }
             else if (mes == 12)
             {
-                return "Dezembro";
+                return "dezembro";
             }
             return "Invalido.";
         }
-      
+        public string ObterDiaPorExtenso()
+        {
+            var dia = Data.Day;
+
+            if (dia == 1)
+            {
+                return "Primeiro de ";
+            }
+            else if (dia == 2)
+            {
+                return "Dois de ";
+            }
+            else if (dia == 3)
+            {
+                return "Três de ";
+            }
+            else if (dia == 4)
+            {
+                return "Quatro de";
+            }
+            else if (dia == 5)
+            {
+                return "Cinco de";
+            }
+            else if (dia == 6)
+            {
+                return "Seis de";
+            }
+            else if (dia == 7)
+            {
+                return "Sete de";
+            }
+            else if (dia == 8)
+            {
+                return "Oito de";
+            }
+            else if (dia == 9)
+            {
+                return "Nove de";
+            }
+
+            return "Ivalido";
+        }
+        public string ObterAnoPorExtenso()
+        {
+            var ano = Data.Year;
+            
+            return "Invatido";
+        }
 
     }
 }
