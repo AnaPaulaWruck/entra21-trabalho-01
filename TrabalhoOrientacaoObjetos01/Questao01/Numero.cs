84c633400592add81c8ec6f8f69490ccd0188145
Ana Paula Wruck <wruck.ap@gmail.com>
2022-06-02 01:30:20 -0300

    Alterada classe Numero Feito código base para obter matematicamente os valores de milhar até centésimo isoladamente.

--- TrabalhoOrientacaoObjetos01/Questao01/Numero.cs
+++ TrabalhoOrientacaoObjetos01/Questao01/Numero.cs
@@ -10,7 +10,7 @@ namespace TrabalhoOrientacaoObjetos01.Questao01
     {
         public double Valor;
 
-        public bool VerificarSeValorAceito()
+        public bool VerificarSeValorValido()
         {
             if (Valor < 0.00 || Valor > 9999.99)
             {
@@ -20,21 +20,39 @@ namespace TrabalhoOrientacaoObjetos01.Questao01
             return true;
         }
 
-        //public bool VerificarSeValorInteiro()
-        //{
-        //    if (VerificarSeValorAceito() == true)
-        //    {
-        //        string valor = Valor.ToString();
-        //        if (valor.Contains(".") || valor.Contains(","))
-        //        {
-        //            return false;
-        //        }
-        //    }
+		/* double valor = 1645.32;
 
-        //    return false;
-        //}
+		double milhar = (valor - (valor % 1000)) / 1000;
+		valor = Math.Round((valor % 1000), 2);
+		Console.WriteLine(milhar);
+		Console.WriteLine(valor);
+		
+		double centena = (valor - (valor % 100)) / 100;
+		valor = Math.Round((valor % 100), 2);
+		Console.WriteLine(centena);
+		Console.WriteLine(valor);
+		
+		double dezena = (valor - (valor % 10)) / 10;
+		valor = Math.Round((valor % 10), 2);
+		Console.WriteLine(dezena);
+		Console.WriteLine(valor);
+		
+		double unidade = (valor - (valor % 1)) / 1;
+		valor = Math.Round((valor % 1), 2);
+		Console.WriteLine(unidade);
+		Console.WriteLine(valor);
+		
+		double decimo = (valor - (valor % 0.1)) / 0.1;
+		valor = Math.Round((valor % 0.1), 2);
+		Console.WriteLine(decimo);
+		Console.WriteLine(valor);
+		
+		double centesimo = (valor - (valor % 0.01)) / 0.01;
+		valor = Math.Round((valor % 0.01), 2);
+		Console.WriteLine(centesimo);
+		Console.WriteLine(valor);
 
-
+		*/
     }
 }
 
