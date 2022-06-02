43c8a009fc1dfc9d23e9a6c1797bd63d1b4636ee
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-06-02 18:44:57 -0300

    Criação de IF's  e ELSE's.

--- TrabalhoOrientacaoObjetos01/Questao03/Relogio.cs
+++ TrabalhoOrientacaoObjetos01/Questao03/Relogio.cs
@@ -8,13 +8,60 @@ namespace TrabalhoOrientacaoObjetos01.Questao03
 {
     public class Relogio
     {
-        public string Hora, Minuto, Segundo, HoraCompleta, Milhar, Centena, Dezena;
-        //public string ObterHoraPorExtenso()
-        //{
-        //    string hora = ;
+        public string Hora, Minuto, Segundo, HoraCompleta, Milhar, Centena, Dezena, Unidade;
+        public string ObterHoraPorExtensoEmDezena()
+        {
+            if (Dezena == "1")
+                Dezena = "Dez";
 
-        //    return hora;
-        //}
+            else if (Dezena == "2")
+                Dezena = "Vinte";
+
+            else if (Dezena == "3")
+                Dezena = "Trinta";
+
+            else if (Dezena == "4")
+                Dezena = "Quarenta";
+
+            else if (Dezena == "5")
+                Dezena = "Cinquenta";
+
+            else if (Dezena == "6")
+                Dezena = "";
+
+            return Dezena;
+        }
+        public string ObterHoraPorExtensoEmUnidade()
+        {
+            if (Unidade == "1")
+                Unidade = "Um";
+
+            else if (Unidade == "2")
+                Unidade = "Dois";
+
+            else if (Unidade == "3")
+                Unidade = "Três";
+
+            else if (Unidade == "4")
+                Unidade = "Quatro";
+
+            else if (Unidade == "5")
+                Unidade = "Cinco";
+
+            else if (Unidade == "6")
+                Unidade = "Seis";
+
+
+            
+            return Unidade;
+        }
+        public string ObterMinutoPorExtensoEmDezena()
+        {
+            if (Dezena == "1")
+                Dezena = "Dez";
+
+            return Dezena;
+        }
 
 
     }
