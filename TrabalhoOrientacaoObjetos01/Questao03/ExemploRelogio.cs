9ba1ae19b1acc84e7ea876f5c0492778c71451d6
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-06-01 21:42:20 -0300

    Addicionamento Da Parte 03: Hora

---
+++ TrabalhoOrientacaoObjetos01/Questao03/ExemploRelogio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoOrientacaoObjetos01.Questao03
+{
+    public class ExemploRelogio
+    {
+        public void Executar()
+        {
+            var relogio = new Relogio();
+            relogio.Hora = 13;
+            relogio.Minuto = 32;
+            relogio.Segundo = 59;
+            relogio.HoraCompleta = "21:32:59";
+
+
+
+
+        }
+    }
+}
---
+++ TrabalhoOrientacaoObjetos01/Questao03/Relogio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoOrientacaoObjetos01.Questao03
+{
+    public class Relogio
+    {
+        public DateTime Hora, Minuto, Segundo, HoraCompleta;
+        public DateTime ObterHoraPorExtenso()
+        {
+            DateTime hora = Hora;
+
+            return hora;
+        }
+
+    }
+}
