4fa0676d45ded1086461048ac0c2a4a5b5a58a8c
Amanda Caroline Bauler Mota <amanda.bauler@hotmail.com>
2022-06-04 22:59:34 -0300

    Finalização das questões 02 e 03.

--- TrabalhoOrientacaoObjetos01/Questao03/ExecutarRelogio.cs
+++ TrabalhoOrientacaoObjetos01/Questao03/ExecutarRelogio.cs
@@ -7,13 +7,66 @@ using System.Threading.Tasks;
 namespace TrabalhoOrientacaoObjetos01.Questao03
 {
     // Guilherme Dzesigaleski Mueller
+    // Participação especial Amanda Bauler.
     public class ExecutarRelogio
     {
         public void Executar()
-        { 
-            DateTime relogio = DateTime.Parse("19:37:47");
-            relogio.ToString("19:37:47");
-            Console.Write(relogio);
+        {
+            Console.Clear();
+            var opcaoDesejada = 0;
+
+            var relogio = new Relogio();
+
+            while (opcaoDesejada != 5)
+            {
+
+                Console.WriteLine(@"-------MENU-------
+            1 - OBTER HORA POR EXTENSO
+            2 - OBTER MINUTO POR EXTENSO 
+            3 - OBTER SEGUNDO POR EXTENSO
+            4 - OBTER HORARIO COMPLETO POR EXTENSO
+            5 - SAIR");
+
+                Console.WriteLine("Escolha a opção desejada: ");
+                opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+                if (opcaoDesejada == 1)
+                {
+                    Console.WriteLine("Digite um horario: ");
+                    DateTime horaEscolhida = Convert.ToDateTime(Console.ReadLine());
+
+                    relogio.Hora = horaEscolhida;
+
+                    Console.WriteLine("A hora é: " + relogio.ObterHoraPorExtenso());
+                }
+                else if (opcaoDesejada == 2)
+                {
+                    Console.WriteLine("Digite um horario: ");
+                    DateTime horaEscolhida = Convert.ToDateTime(Console.ReadLine());
+
+                    relogio.Hora = horaEscolhida;
+
+                    Console.WriteLine("O minuto é: " + relogio.ObterMinutoPorExtenso());
+                }
+                else if (opcaoDesejada == 3)
+                {
+                    Console.WriteLine("Digite um horario: ");
+                    DateTime horaEscolhida = Convert.ToDateTime(Console.ReadLine());
+
+                    relogio.Hora = horaEscolhida;
+
+                    Console.WriteLine("O segundo é: " + relogio.ObterSegundoPorExtenso());
+                }
+                else if (opcaoDesejada == 4)
+                {
+                    Console.WriteLine("Digite um horario: ");
+                    DateTime horaEscolhida = Convert.ToDateTime(Console.ReadLine());
+
+                    relogio.Hora = horaEscolhida;
+
+                    Console.WriteLine("Horario completo: " + relogio.ApresentarHoraPorExtenso());
+                }
+
+            }
         }
     }
 }
